c1a48ac78735d819441bf3026e61c00f445b44fa
michaelkacos <mkacos@peraton.com>
2024-04-11 08:59:36 -0400

    Added a min length to required field

--- Sources/SwiftFoundationPackage/RequiredTextField.swift
+++ Sources/SwiftFoundationPackage/RequiredTextField.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 public struct RequiredTextField: View {
     public var title: String
+    public var minLength: Int = 0
     @Binding public var valueToBindTo: String
     
     public init(title: String, valueToBindTo: Binding<String>) {
@@ -18,8 +19,8 @@ public struct RequiredTextField: View {
     
     public var body: some View {
         TextField(title, text: $valueToBindTo).clearButton(text: $valueToBindTo)
-            .padding(  valueToBindTo.isEmpty ? 6 : 0)
-            .border(valueToBindTo.isEmpty ? Color.red : Color.clear)
+            .padding(  valueToBindTo.isEmpty || (minLength != 0 && valueToBindTo.count < minLength) ? 6 : 0)
+            .border(valueToBindTo.isEmpty || (minLength != 0 && valueToBindTo.count < minLength) ? Color.red : Color.clear)
     }
 }
 
