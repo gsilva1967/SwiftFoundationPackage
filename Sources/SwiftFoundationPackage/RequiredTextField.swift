530977231f8a13d51bfda0bb9a7c67b224d8837d
michaelkacos <mkacos@peraton.com>
2024-09-06 09:48:10 -0400

    Changed requiredtextfield (and added optional textfield) to display titles differently

--- Sources/SwiftFoundationPackage/RequiredTextField.swift
+++ Sources/SwiftFoundationPackage/RequiredTextField.swift
@@ -1,13 +1,12 @@
 //
 //  RequiredTextField.swift
-//  MobileGoRecruit
 //
 //  Created by Michael Kacos on 9/1/23.
 //
 
 import SwiftUI
 
-public struct RequiredTextField: View {
+public struct RequiredTextField2: View {
     public var title: String
     public var minLength: Int = 0
     @Binding public var valueToBindTo: String
@@ -19,9 +18,17 @@ public struct RequiredTextField: View {
     }
     
     public var body: some View {
-        TextField(title, text: $valueToBindTo).clearButton(text: $valueToBindTo)
-            .padding(  valueToBindTo.isEmpty || (minLength != 0 && valueToBindTo.count < minLength) ? 6 : 0)
-            .border(valueToBindTo.isEmpty || (minLength != 0 && valueToBindTo.count < minLength) ? Color.red : Color.clear)
+        VStack{
+            HStack{
+                Text(title)
+                    .font(.caption2)
+                    .foregroundColor(.secondary)
+                Spacer()
+            }
+            TextField(title, text: $valueToBindTo).clearButton(text: $valueToBindTo)
+                .padding(  valueToBindTo.isEmpty || (minLength != 0 && valueToBindTo.count < minLength) ? 6 : 0)
+                .border(valueToBindTo.isEmpty || (minLength != 0 && valueToBindTo.count < minLength) ? Color.red : Color.clear)
+        }
     }
 }
 
