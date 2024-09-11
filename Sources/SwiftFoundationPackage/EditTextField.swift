e9dbb5c9bd0f27fb94b6431bb1286dec7a72a311
michaelkacos <mkacos@peraton.com>
2024-09-11 09:00:17 -0400

    Renamed textfield and added placeholder

--- Sources/SwiftFoundationPackage/EditTextField.swift
+++ Sources/SwiftFoundationPackage/EditTextField.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  EditTextField.swift
 //  
 //
 //  Created by Michael Kacos on 9/9/24.
@@ -9,14 +9,16 @@ import Foundation
 
 import SwiftUI
 
-public struct FoundationTextField: View {
+public struct EditTextField: View {
     public var title: String
+    public var placeholderText: String = ""
     public var minLength: Int = 0
     public var isRequired: Bool = false
     @Binding public var valueToBindTo: String
     
-    public init(title: String, valueToBindTo: Binding<String>, minLength: Int = 0, isRequired: Bool = false) {
+    public init(title: String, placeholderText: String = "",valueToBindTo: Binding<String>, minLength: Int = 0, isRequired: Bool = false) {
         self.title = title
+        self.placeholderText = placeholderText.isEmpty ? title : placeholderText
         self.minLength = minLength
         self._valueToBindTo = valueToBindTo
         self.isRequired = isRequired
@@ -30,7 +32,7 @@ public struct FoundationTextField: View {
                     .foregroundColor(.secondary)
                 Spacer()
             }
-            TextField(title, text: $valueToBindTo).clearButton(text: $valueToBindTo)
+            TextField(placeholderText, text: $valueToBindTo).clearButton(text: $valueToBindTo)
                 .padding(isRequired == false ? 0 : valueToBindTo.isEmpty || (minLength != 0 && valueToBindTo.count < minLength) ? 6 : 0)
                 .border(isRequired == false ? Color.clear : valueToBindTo.isEmpty || (minLength != 0 && valueToBindTo.count < minLength) ? Color.red : Color.clear)
         }
--- Sources/SwiftFoundationPackage/OptionalTextField.swift
+++ Sources/SwiftFoundationPackage/OptionalTextField.swift
@@ -5,28 +5,28 @@
 //  Created by Michael Kacos on 9/6/24.
 //
 
-import SwiftUI
-
-public struct OptionalTextField: View {
-    public var title: String
-    @Binding public var text: String
-    
-    public init(title: String, text: Binding<String>) {
-        self.title = title
-        self._text = text
-    }
-    
-    public var body: some View {
-        VStack{
-            HStack{
-                Text(title)
-                    .font(.caption2)
-                    .foregroundColor(.secondary)
-                Spacer()
-            }
-            TextField(title, text: $text).clearButton(text: $text)
-                
-        }
-    }
-}
+//import SwiftUI
+//
+//public struct OptionalTextField: View {
+//    public var title: String
+//    @Binding public var text: String
+//    
+//    public init(title: String, text: Binding<String>) {
+//        self.title = title
+//        self._text = text
+//    }
+//    
+//    public var body: some View {
+//        VStack{
+//            HStack{
+//                Text(title)
+//                    .font(.caption2)
+//                    .foregroundColor(.secondary)
+//                Spacer()
+//            }
+//            TextField(title, text: $text).clearButton(text: $text)
+//                
+//        }
+//    }
+//}
 
