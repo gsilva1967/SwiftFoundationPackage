a9d01362cb2ca846651ccff2733c2f8d24de6969
michaelkacos <mkacos@peraton.com>
2024-10-02 09:04:23 -0400

    removed the padding that didn't help

--- Sources/SwiftFoundationPackage/TextField+ClearButton.swift
+++ Sources/SwiftFoundationPackage/TextField+ClearButton.swift
@@ -21,7 +21,7 @@ public struct ClearButton: ViewModifier {
                 } label: {
                     Image(systemName: "multiply.circle.fill")
                         .foregroundStyle(.gray)
-                        .padding(8)
+                        
                         
                 }
                 .padding(.trailing, 4)
