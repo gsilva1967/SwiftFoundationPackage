02f3fa5a1e1b30b139edb4c47c68911c40dcd53d
michaelkacos <mkacos@peraton.com>
2025-01-09 11:00:58 -0500

    Moved x position to 24

--- Sources/SwiftFoundationPackage/BadgeView.swift
+++ Sources/SwiftFoundationPackage/BadgeView.swift
@@ -11,7 +11,7 @@ public struct BadgeView : View {
     
     @Binding public var value: Int
     
-    @State public var x = 30.0
+    @State public var x = 24.0
     @State public var y = 4.0
     @State public var foreground: Color = .white
     @State public var background: Color = .red
